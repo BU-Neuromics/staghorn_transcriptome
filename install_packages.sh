d28a744615139a864bafbd750ccdf25289284172
Adam Labadorf <alabadorf@gmail.com>
2020-07-08 12:47:55 -0400

    software package update

--- install_packages.sh
+++ install_packages.sh
@@ -1,3 +1,3 @@
-conda install -c bioconda -c conda-forge graphviz
+conda install -c bioconda -c conda-forge graphviz salmon=1.1.0
 
-pip install biopython snakemake pysam pygtrie pydot holoviews
+pip install biopython snakemake pysam pygtrie pydot holoviews hvplot de-toolkit
