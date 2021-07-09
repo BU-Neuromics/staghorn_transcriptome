6811d5354ee59db81d6531d68a3a7905f2f12198
Adam Labadorf <alabadorf@gmail.com>
2021-07-09 14:27:00 -0400

    package install script update

--- install_packages.sh
+++ install_packages.sh
@@ -1,3 +1,6 @@
 conda install -c bioconda -c conda-forge graphviz salmon=1.1.0
 
-pip install biopython snakemake pysam pygtrie pydot holoviews hvplot de-toolkit
+# for exporting holoviews graphs with bokeh backend to png
+conda install -c conda-forge firefox geckodriver
+
+pip install biopython snakemake pysam pygtrie pydot holoviews hvplot de-toolkit selenium
